632a3f393bb412eeea38843b490c73d7c31e04b4
Ari Garcia <aristofanio@hotmail.com>
2017-05-05 10:56:34 -0300

    convertendo para maven project

--- async-clientapp/src/ifpb/Main.java
+++ async-clientapp/src/ifpb/Main.java
@@ -8,16 +8,18 @@ import java.rmi.registry.Registry;
 public class Main {
 
 	public static void main(String[] args) throws RemoteException, NotBoundException {
-		//
+		//log
 		System.out.println("Acionado o clientapp");
-		//
+		//recuperação do Sender
 		Registry registry = LocateRegistry.getRegistry(10990);
 		ISender sender = (ISender) registry.lookup("Sender");
 		//
-		String id = "askjdlkasjd-alksdj";
+		String id = "askjdlkasjd";
 		String text = "Hello World!";
 		//
 		sender.sendMessage(new Message(id, text));
+		//recuperar uma resposta
+		//??????
 	}
 	
 }
